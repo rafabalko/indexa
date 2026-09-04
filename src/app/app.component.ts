8e7cbb4844b70d3ba2c2a772869ee9db4985e3e8
Rafael <rafaelbalko@gmail.com>
2026-09-04 20:45:43 -0300

    versão final

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+import agenda from './agenda.json';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.contatos.filter(contato => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
+}
---
+++ src/app/componentes/separador/separador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-separador',
+  standalone: true,
+  imports: [],
+  templateUrl: './separador.component.html',
+  styleUrl: './separador.component.css'
+})
+export class SeparadorComponent {}
